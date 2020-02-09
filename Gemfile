9dec881898d0b70bead309e7a83062eeef42eb1e
TakahitoYasuhara <ssct36810@gmail.com>
2020-02-09 05:19:17 +0000

    [Add]rails-18nの導入

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem 'slim-rails', '3.1.3'
 
 gem 'sorcery'
 
+gem 'rails-i18n'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,17 @@ module CuriosityApp
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    
+    # 言語ファイルを階層ごとに設定するための記述
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
+    # アプリケーションが対応している言語のホワイトリスト(ja = 日本語, en = 英語)
+    config.i18n.available_locales = %i(ja en)
+
+    # 上記の対応言語以外の言語が指定された場合、エラーとするかの設定
+    config.i18n.enforce_available_locales = true
+
+    # デフォルトの言語設定
+    config.i18n.default_locale = :ja
   end
 end
