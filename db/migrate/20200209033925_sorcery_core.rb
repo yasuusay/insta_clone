6eeb814f5699f3ecece4d1753f1e96dd0d660a06
TakahitoYasuhara <ssct36810@gmail.com>
2020-02-09 11:40:03 +0000

    [Add]ユーザー登録機能の実装

--- db/migrate/20200209033925_sorcery_core.rb
+++ db/migrate/20200209033925_sorcery_core.rb
@@ -4,6 +4,7 @@ class SorceryCore < ActiveRecord::Migration[5.2]
       t.string :email,            null: false
       t.string :crypted_password
       t.string :salt
+      t.string :username, null: false
 
       t.timestamps                null: false
     end
