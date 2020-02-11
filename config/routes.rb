6c263b4e59e6d871f74d3ee78cf657c88d319e74
TakahitoYasuhara <ssct36810@gmail.com>
2020-02-11 01:33:02 +0000

    [Add]ログイン/ログアウト機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
     root 'users#new'
+    resources :users, only: [:new, :create, :show]
     
-    resources :users, only: [:new, :create]
+    get 'login', to: 'user_sessions#new'
+    post 'login', to: 'user_sessions#create'
+    post 'logout', to: 'user_sessions#destroy'
 end
