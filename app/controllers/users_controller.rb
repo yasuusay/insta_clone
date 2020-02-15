e4cc33ed76a9556b915703832142cee77b938fc9
TakahitoYasuhara <ssct36810@gmail.com>
2020-02-15 01:53:20 +0000

    [Add]ユーザー登録後にログイン

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash.now[:success] = 'ユーザーを作成しました'
-      redirect_to @user
+      auto_login(@user)
+      redirect_to @user, success: 'ユーザーを作成しました'
     else
       flash.now[:danger] = 'ユーザーの作成に失敗しました'
       render :new
