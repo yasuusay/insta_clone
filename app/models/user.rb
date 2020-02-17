5e4df9d172dd2f6546b8c7e773e1ba6070680211
TakahitoYasuhara <ssct36810@gmail.com>
2020-02-17 14:25:49 +0000

    [Add]バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,7 @@
 
 class User < ApplicationRecord
   authenticates_with_sorcery!
+  has_many :posts, dependent: :destroy
 
   before_save { self.email = email.downcase }
   validates :password, length: { minimum: 4 }, if: -> { new_record? || changes[:crypted_password] }
